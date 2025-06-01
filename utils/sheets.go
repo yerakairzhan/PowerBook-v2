e46f8c0bab39ebda2af645047f92ca45a3b43f30
yerakairzhan <erakairzhan@gmail.com>
2025-06-01 07:54:22 +0500

    month2

--- utils/sheets.go
+++ utils/sheets.go
@@ -59,7 +59,7 @@ func GetSheetname(time time.Time) string {
 
 func AddUserToSheet(spreadsheetId, userID, username string) error {
 	currentTime := time.Now()
-	currentTime = currentTime.Add(5 * time.Hour)
+	//currentTime = currentTime.Add(5 * time.Hour)
 	sheetName := GetSheetname(currentTime)
 
 	creds := Creds
@@ -108,7 +108,7 @@ func AddUserToSheet(spreadsheetId, userID, username string) error {
 
 func AddReadingMinutes(spreadsheetId, userID string, minutes int, currentTime1 time.Time) error {
 	currentTime := time.Now()
-	currentTime = currentTime.Add(5 * time.Hour)
+	//currentTime = currentTime.Add(5 * time.Hour)
 	sheetName := GetSheetname(currentTime)
 
 	LoadConfig()
@@ -194,7 +194,7 @@ func AddReadingMinutes(spreadsheetId, userID string, minutes int, currentTime1 t
 
 func DeleteUserFromSheet(spreadsheetId, userID string) error {
 	currentTime := time.Now()
-	currentTime = currentTime.Add(5 * time.Hour)
+	//currentTime = currentTime.Add(5 * time.Hour)
 	sheetName := GetSheetname(currentTime)
 
 	creds := Creds
